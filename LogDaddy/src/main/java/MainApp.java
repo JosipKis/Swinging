e5467aafa07df646fca026de1933c9be5730f906
JosipKis <josip.kis2@gmail.com>
2024-11-11 07:58:47 +0100

    Log Manager for a client, initial commit

---
+++ LogDaddy/src/main/java/MainApp.java
@@ -0,0 +1,15 @@
+import Model.MainFrame;
+
+import javax.swing.*;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame();
+            }
+        });
+    }
+}
---
+++ LogDaddy/src/main/java/Model/MainFrame.java
@@ -0,0 +1,29 @@
+package Model;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        super("Log Manager");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        initComps();
+        layoutComps();
+        activateComps();
+    }
+
+    private void initComps() {
+
+    }
+
+    private void layoutComps() {
+
+    }
+
+    private void activateComps() {
+
+    }
+}
